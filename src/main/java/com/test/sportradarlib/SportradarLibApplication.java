b88ed2cd94d57d7b0df6182f099333819406c1ec
Sebastian Blanco <sebastianblancogonz@gmail.com>
2020-11-29 08:56:29 +0100

    Import first files

---
+++ src/main/java/com/test/sportradarlib/SportradarLibApplication.java
@@ -0,0 +1,13 @@
+package com.test.sportradarlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportradarLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportradarLibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/sportradarlib/SportradarLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.sportradarlib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportradarLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
